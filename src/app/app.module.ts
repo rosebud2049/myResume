673c898188ebfd62bfb627ab00fa204a6be8fa55
raf <raphael.palerme@epitech.eu>
2018-12-19 20:18:55 +0100

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { ResumeComponent } from './resume/resume.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    ResumeComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
